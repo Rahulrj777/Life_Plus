d19b7369ec024a8e0d7b5eced668b01764860f7b
Rahul Srinivasan J <rahulsrinivasannkl@gmail.com>
2025-08-06 12:17:52 +0530

    Initial project setup with frontend code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,13 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.png'
 
-// import logo from '../images/64.png'
-import logo from '../images/footer_logo.png'
-// import line from '../images/banner/line.png'
-
-
-//icons
 import { FiInstagram } from "react-icons/fi";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { ImWhatsapp } from "react-icons/im";
 import { PiLinkedinLogoBold } from "react-icons/pi";
 import { LuFacebook } from "react-icons/lu";
-import { FaThreads } from "react-icons/fa6";
 import { MdOutlinePhoneAndroid } from "react-icons/md";
-import { TbDeviceLandlinePhone } from "react-icons/tb";
-
 
 import { MdOutlineMail } from "react-icons/md";
-import { FaMobileScreen } from "react-icons/fa6";
-
-
-
-
-
 
 const Footer = () => {
 
@@ -40,7 +24,7 @@ const Footer = () => {
                     <div class="px-4  w-full  md:w-[85%] mx-auto md:px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 ">
 
                         <div class=" p-2 md:p-4 ">
-                            <img src={logo} className='w-14 sm:w-14 md:w-32  object-cover ' alt="" />
+                            <img src={logo} className='w-14 sm:w-14 md:w-38  object-cover ' alt="" />
 
                             <p className='mt-3 font-[Merriweather]  text-[11px] leading-5 text-white font-extralight'>Discover holistic wellness with PranaVeda's expert Ayurveda services and transform your health naturally.</p>
 
@@ -85,12 +69,10 @@ const Footer = () => {
 
 
                             <div >
-                                {/* <h2 className='text-[14px] font-semibold '>SPECTRA CONSTRUCTIONS AND INFRASTRUCTURE PVT. LTD</h2> */}
                                 <p class="text-gray-100 my-3 block  text-[12px] sm:text-[13px]  md:text-[13px]" >  Chennai  </p>
 
                             </div>
                             <a href="mailto::lifeplusayushvadhyasala@gmail.com" target="_blank" class="text-gray-100 my-3   text-[12px] sm:text-[13px]  md:text-[13px] flex items-center gap-x-2" ><MdOutlineMail className=' text-[16px] md:text-[20px] font-bold' /> lifeplusayushvadhyasala@gmail.com</a>
-                            {/* <a href="tel:91" target="_blank" className='text-gray-100 text-[12px] sm:text-[13px] md:text-[14px] my-3 flex items-center gap-x-2 '><MdOutlinePhoneAndroid className='text-[16px] md:text-[20px] font-bold' /> 044 -22602050</a> */}
                             <a href="tel:918015907797" target="_blank" className='text-gray-100 text-[12px] sm:text-[13px] md:text-[13px] my-3 flex items-center gap-x-2 '><MdOutlinePhoneAndroid className='text-[16px] md:text-[20px] font-bold' /> +91 8015907797</a>
                             <a href="tel:918015525797" target="_blank" className='text-gray-100 text-[12px] sm:text-[13px] md:text-[13px] my-3 flex items-center gap-x-2 '><MdOutlinePhoneAndroid className='text-[16px] md:text-[20px] font-bold' /> +91 8015525797</a>
                             <a href="tel:918015508797" target="_blank" className='text-gray-100 text-[12px] sm:text-[13px] md:text-[13px] my-3 flex items-center gap-x-2 '><MdOutlinePhoneAndroid className='text-[16px] md:text-[20px] font-bold' /> +91 8015508797</a>
@@ -120,16 +102,6 @@ const Footer = () => {
                                     </div>
                                 </Link>
 
-                                {/* <Link to="https://api.whatsapp.com/send?phone="
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className=""
-                                >
-                                    <div className='w-6 h-6 md:w-9 md:h-9 bg-white hover:border hover:border-[#6bb8ff] hover:bg-[#fbfbfb] rounded-full flex justify-center items-center shadow-md drop-shadow-md cursor-pointer hover:shadow-lg hover:drop-shadow-xl hover:animate-bounce'>
-                                        <ImWhatsapp className='text-[12px] sm:text-[13px] md:text-[16px] text-green-800' />
-                                    </div>
-                                </Link> */}
-
                                 <Link to="" target='_blank' className="">
                                     <div className='w-6 h-6 md:w-9 md:h-9 bg-white hover:border hover:border-[#6bb8ff] hover:bg-[#fbfbfb] rounded-full flex justify-center items-center shadow-md drop-shadow-md  cursor-pointer hover:shadow-lg hover:drop-shadow-xl hover:animate-bounce'>
                                         <PiLinkedinLogoBold className='text-[12px] sm:text-[13px] md:text-[16px] text-[#8e6600]' />
@@ -141,12 +113,6 @@ const Footer = () => {
                                         <LuFacebook className='text-[12px] sm:text-[13px] md:text-[16px] text-[#8e6600]' />
                                     </div>
                                 </Link>
-
-                                {/* <Link to="" target='_blank'>
-                                    <div className='w-6 h-6 md:w-9 md:h-9 bg-white hover:border hover:border-gray-300 hover:bg-[#fbfbfb] rounded-full flex justify-center items-center shadow-md drop-shadow-md cursor-pointer hover:shadow-lg hover:drop-shadow-xl hover:animate-bounce'>
-                                        <FaThreads className='text-[12px] sm:text-[13px] md:text-[16px] text-gray-800' />
-                                    </div>
-                                </Link> */}
                             </div>
                         </div>
 
